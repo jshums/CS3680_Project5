a08b8b6ca287b15e89e24d5058bf06094781db4e
Unknown <jubleshum@gmail.com>
2017-03-07 16:13:17 -0700

    Created Project

---
+++ app/src/main/java/com/cs3680/justin/js_project5_networkdata/NetworkDataActivity.java
@@ -0,0 +1,13 @@
+package com.cs3680.justin.js_project5_networkdata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NetworkDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_network_data);
+    }
+}
